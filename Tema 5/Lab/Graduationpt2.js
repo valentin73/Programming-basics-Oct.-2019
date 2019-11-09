6b2beccf5207cf3709956392cc476a5406be812e
Valentin Gelovski <gelovski.valentin@gmail.com>
2019-11-10 00:38:14 +0200

    l

--- Tema 5/Lab/Graduationpt2.js
+++ Tema 5/Lab/Graduationpt2.js
@@ -5,21 +5,27 @@ function graduation2(input) {
     let gradeSum = 0;
 
     let badGradeCounter = 0;
+    let flag = false;
     while (counter <= 12) {
         let grade = Number(input.shift());
+
         if (grade < 4) {
             badGradeCounter++;
-            if (badGradeCounter === 2) {
+            if (badGradeCounter == 2) {
                 console.log(`${name} has been excluded at ${counter} grade`);
+                flag = true;
                 break;
-
-            } else {
-                gradeSum += grade;
-                counter++;
-                let avarageSum = gradeSum / 12;
-                console.log(`${name} graduated. Avarage grade: ${avarageSum.toFixed(2)}`)
             }
+        } else {
+            gradeSum += grade;
+            counter++;
         }
-        
+    }
+    if(!flag){
+    let averageSum = gradeSum / 12;
+    console.log(`${name} graduated. Average grade: ${averageSum.toFixed(2)}`)
     }
 }
+
+graduation2(["Pesho",   4,  5.5,  6,  5.43,  4.5,  6,  5.55,  5,  6,  6, 5.43,5, 6,])
+graduation2(["Mimi", 5, 6, 5, 6, 5, 6, 6, 2, 3,])
